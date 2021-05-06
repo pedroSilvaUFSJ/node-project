d1468959a269fdc95e4240423d178e75c41263c0
Pedro Henrique da Silva <pedro.silva@bluepanda.pt>
2021-05-06 10:38:31 +0100

    moving sensitive information to .env logic and putting a sample on env_file

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+const { mongoUrl } = require('./../.env')
+
 mongoose
-    .connect('mongodb://localhost/knowledge_stats', { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(e => {
         const msg = 'ERRO! Não foi possível conectar com o MongoDB!'
         console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,12 +1,8 @@
+const { db } = require('./.env')
+
 module.exports = {
 	client: 'postgresql',
-	connection: {
-		host: '127.0.0.1',
-		port: 5432,
-		database: 'knowledge',
-		user: 'postgres',
-		password: ' ',
-	},
+	connection: db,
 	pool: { min: 2, max: 10 },
 	migrations: { tableName: 'knex_migrations' }
 };
