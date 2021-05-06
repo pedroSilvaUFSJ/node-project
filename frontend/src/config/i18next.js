9e150757f5fdb2fe57bfa5607189c3c1a86d4d8c
Pedro Henrique da Silva <pedro.silva@bluepanda.pt>
2021-05-06 15:08:27 +0100

    creating main layout

--- frontend/src/config/i18next.js
+++ frontend/src/config/i18next.js
@@ -1,13 +1,12 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import translationEN from "assets/locales/en/translation.json";
-import translationPT from "assets/locales/pt/translation.json";
+import VueI18Next from 'vue-i18next';
+import translationEN from "@/locales/en/translation.json";
+import translationPT from "@/locales/pt/translation.json";
 
 const fallbackLng = ['en'];
 const availableLanguages = ['en', 'pt'];
-
 const resources = {
   en: { translation: translationEN },
   pt: { translation: translationPT }
@@ -16,10 +15,10 @@ const resources = {
 i18n
   .use(Backend) // load translations using http (default                                               public/assets/locals/en/translations)
   .use(LanguageDetector) // detect user language
-  .use(initReactI18next) // pass the i18n instance to react-i18next.
+  .use(VueI18Next) // pass the i18n instance to react-i18next.
   .init({
-    resources,
     fallbackLng, // fallback language is english.
+    resources,
     detection: { checkWhitelist: true }, // options for language detection
     debug: false,
     whitelist: availableLanguages,
