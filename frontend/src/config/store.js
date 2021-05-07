38990014382da4d72a2d615c403c67ed7504b697
Pedro Henrique da Silva <pedro.silva@bluepanda.pt>
2021-05-07 09:37:17 +0100

    including UserDropdown on navbar

--- frontend/src/config/store.js
+++ frontend/src/config/store.js
@@ -6,6 +6,11 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     state: {
         isMenuVisible: true,
+        user: {
+            name: 'Pedro Henrique da Silva',
+            email: 'mock@cod3r.com.br',
+            admin: true
+        }
     },
     mutations: {
         toggleMenu(state, isVisible) {
