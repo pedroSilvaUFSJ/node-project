cdb31391fcec0340164e94875b62dc59eec3ae89
Pedro Henrique da Silva <pedro.silva@bluepanda.pt>
2021-07-16 14:16:50 +0100

    creating article list

--- frontend/src/config/router.js
+++ frontend/src/config/router.js
@@ -3,12 +3,16 @@ import VueRouter from 'vue-router'
 
 import Home from '@/components/home/Home'
 import AdminPages from '@/components/admin/AdminPages'
+import ArticlesByCategory from '@/components/article/ArticleByCategory'
+import ArticleById from '@/components/article/ArticleById'
 
 Vue.use(VueRouter)
 
 const routes = [
     { name: 'home', path: '/', component: Home },
-    { name: 'adminPages', path: '/admin', component: AdminPages, meta: { requiresAdmin: true } }
+    { name: 'adminPages', path: '/admin', component: AdminPages, meta: { requiresAdmin: true } },
+    { name: 'articlesByCategory', path: '/categories/:id/articles', component: ArticlesByCategory, meta: { requiresAdmin: false } },
+    { name: 'articleById', path: '/articles/:id', component: ArticleById, meta: { requiresAdmin: false } }
 ]
 
 const router = new VueRouter({ mode: 'history', routes })
